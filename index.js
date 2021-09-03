44bb98d10afb70448c8cd45aa3e2f25265bf4d6f
Do Hun Lee <leedh7@kaist.ac.kr>
2021-09-03 17:45:29 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended : true}));
+//applictionjson
+app.use(bodyParser.json());
 
-const mongoose = require('mongoose')
+const { User } = require("./models/User");
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://dohunlee:dohunlee@boilerplate.yacee.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=>console.log("MongoDB Connected...")).catch(err => console.log(err))
+mongoose.connect(config.mongoURI).then(()=>console.log("MongoDB Connected...")).catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! ~안녕하세요 새해복 많이 받으세요 하이')
+})
+
+app.post('/register', (req,res) => {
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면 데이터 베이스에 넣는 작업
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if(err) return res.json({ success : false, err});
+    return res.status(200).json({success : true});
+  })
 })
 
 app.listen(port, () => {
